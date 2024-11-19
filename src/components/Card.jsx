a69c3a0ed61587c32445b8210a6a716b59a0c5d5
Prachi3045 <pikugurav007@gmail.com>
2024-11-19 11:18:35 +0530

    Your commit message

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { PRIORITY_MAP, STATUS_MAP } from '../utils/constants';
+import React from "react";
+import { PRIORITY_MAP, STATUS_MAP } from "../utils/constants";
 
 const Card = ({ ticket, users, groupBy }) => {
-  const user = users.find(u => u.id === ticket.userId);
-  
+  const user = users.find((u) => u.id === ticket.userId);
+
   return (
     <div className="ticket-card">
       <div className="card-header">
         <span className="ticket-id">{ticket.id}</span>
-        {groupBy !== 'user' && (
+        {groupBy !== "user" && (
           <div className="user-avatar">
-            <img 
-              src={user?.avatar || '/placeholder.svg'} 
+            <img
+              src={user?.avatar || "/placeholder.svg"}
               alt={user?.name}
               className="avatar-img"
             />
@@ -19,23 +19,23 @@ const Card = ({ ticket, users, groupBy }) => {
         )}
       </div>
       <div className="card-title">
+        {groupBy !== "status" && (
+          <span className="status-tag">
+            <img src={STATUS_MAP[ticket.status].icon} alt="" />
+          </span>
+        )}
         <h3>{ticket.title}</h3>
       </div>
       <div className="card-tags">
-        {groupBy !== 'priority' && (
+        {groupBy !== "priority" && (
           <span className="priority-tag">
             <img src={PRIORITY_MAP[ticket.priority].icon} alt="" />
           </span>
         )}
-        {groupBy !== 'status' && (
-          <span className="status-tag">
-            <img src={STATUS_MAP[ticket.status].icon} alt="" />
-             {ticket.status}
-          </span>
-        )}
+
         <span className="feature-tag">
           <span className="dot">●</span>
-          Feature Request
+          {ticket.tag}
         </span>
       </div>
     </div>
